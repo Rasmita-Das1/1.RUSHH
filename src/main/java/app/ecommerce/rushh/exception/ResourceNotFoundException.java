2cc13eb491a43cb6f6748a5253694a43e1aeb196
Rasmita Das <120312766+Rasmita-Das1@users.noreply.github.com>
2025-06-27 18:09:12 +0530

    Update ResourceNotFoundException.java

--- src/main/java/app/ecommerce/rushh/exception/ResourceNotFoundException.java
+++ src/main/java/app/ecommerce/rushh/exception/ResourceNotFoundException.java
@@ -1,12 +1,5 @@
 package app.ecommerce.rushh.exception;
 
-//public class ResourceNotFoundException extends RuntimeException {
-//
-//    public ResourceNotFoundException(String message) {
-//        super(message);
-//    }
-//}
-
 public class ResourceNotFoundException extends ApiException {
 	
     public ResourceNotFoundException(String message) {
